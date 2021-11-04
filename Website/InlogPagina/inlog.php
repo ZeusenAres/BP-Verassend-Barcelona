b5c54a784f0901611810d351f0fd032b03d9a738
ZeusenAres <2060537@talnet.nl>
2021-11-04 10:21:06 +0100

    css and OOP on web pages

--- Website/InlogPagina/inlog.php
+++ Website/InlogPagina/inlog.php
@@ -13,13 +13,13 @@
             <form action="">
                 <h1>Inloggen voor medewerkers!</h1>
                 <div class="Gebruikersnaam">
-                    <input type="text" placeholder="Gebruikersnaam" required="" id="Gebruikersnaam" />
+                    <input type="text" placeholder="Gebruikersnaam" required="required" id="Gebruikersnaam"/>
                 </div>
                 <div class="Wachtwoord">
-                    <input type="password" placeholder="Wachtwoord" required="" id="Wachtwoord" />
+                    <input type="password" placeholder="Wachtwoord" required="required" id="Wachtwoord"/>
                 </div>
                 <div class="Button">
-                    <input type="submit" value="Log in" id="Button" />
+                    <input type="submit" value="Log in" id="Button"/>
                 </div>
             </form>
         </section>
